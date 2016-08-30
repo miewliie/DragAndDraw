dd53ffb39109bc43a9fe93bfc7f2c9ebeae4c7e3
Your Name <you@example.com>
2016-08-30 12:51:25 +0700

    12:51

---
+++ app/src/main/java/com/augmentis/ayp/dnd/Box.java
@@ -0,0 +1,27 @@
+package com.augmentis.ayp.dnd;
+
+import android.graphics.PointF;
+
+/**
+ * Created by Apinya on 8/30/2016.
+ */
+public class Box {
+    PointF mStart;
+    PointF mEnd;
+
+    public PointF getStart() {
+        return mStart;
+    }
+
+    public void setStart(PointF mStart) {
+        this.mStart = mStart;
+    }
+
+    public PointF getEnd() {
+        return mEnd;
+    }
+
+    public void setEnd(PointF mEnd) {
+        this.mEnd = mEnd;
+    }
+}
--- app/src/main/java/com/augmentis/ayp/dnd/BoxDrawingView.java
+++ app/src/main/java/com/augmentis/ayp/dnd/BoxDrawingView.java
@@ -1,11 +1,16 @@
 package com.augmentis.ayp.dnd;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.PointF;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Apinya on 8/30/2016.
@@ -13,12 +18,41 @@ import android.view.View;
 public class BoxDrawingView extends View{
     private static final String TAG = "BoxDrawingView";
 
+    private Box mCurrentBox;
+    private List<Box> mBoxList = new ArrayList<>();
+    private Paint mBackgroundPaint;
+    private Paint mBoxPaint;
+
     public BoxDrawingView(Context context) {
-        super(context);
+        super(context, null);
     }
 
     public BoxDrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        mBoxPaint = new Paint();
+        mBoxPaint.setColor(0x22ff0000);
+
+        mBackgroundPaint = new Paint();
+        mBackgroundPaint.setColor(0xfff8efe0);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPaint(mBackgroundPaint);
+
+
+        for(Box box : mBoxList){
+
+            float left = Math.min( box.getStart().x, box.getEnd().x );
+            float top = Math.min( box.getStart().y, box.getEnd().y );
+
+            float right = Math.max( box.getStart().x, box.getEnd().x );
+            float bottom = Math.max ( box.getStart().y, box.getEnd().y );
+
+            canvas.drawRect(left, top , right, bottom, mBoxPaint);
+
+        }
     }
 
     @Override
@@ -30,19 +64,43 @@ public class BoxDrawingView extends View{
         switch (event.getAction()){
             case MotionEvent.ACTION_MOVE:
                 action = "ACTION_MOVE";
+
+                if(mCurrentBox != null){
+                    mCurrentBox.setEnd(currentPoint);
+                    invalidate(); //force to re-draw
+                    logBox(mCurrentBox);
+                }
                 break;
-            case MotionEvent.ACTION_UP:
-                action = "ACTION_UP";
-                break;
+
             case MotionEvent.ACTION_DOWN:
                 action = "ACTION_DOWN";
+                mCurrentBox = new Box();
+                mCurrentBox.setStart(currentPoint);
+                mBoxList.add(mCurrentBox);
                 break;
+
+            case MotionEvent.ACTION_UP:
+                action = "ACTION_UP";
+                mCurrentBox = null;
+                break;
+
             case MotionEvent.ACTION_CANCEL:
                 action = "ACTION_CANCEL";
+                mCurrentBox = null;
                 break;
         }
 
-        Log.i(TAG, "Action = " + action + " " + currentPoint.x + "," + currentPoint.y );
+//        Log.i(TAG, "Action = " + action + " " + currentPoint.x + "," + currentPoint.y );
         return true;
+    }
+
+    private void logBox(Box box){
+        if(mCurrentBox != null){
+            Log.d(TAG,
+                    "CurrentBox: "
+            +mCurrentBox.getStart()
+            + " , " + box.getEnd()
+            );
+        }
     }
 }
