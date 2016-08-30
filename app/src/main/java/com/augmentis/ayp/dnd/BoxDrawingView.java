57ee090506205d131affba22da0d544697d74917
Your Name <you@example.com>
2016-08-30 10:23:58 +0700

    st commit wq

---
+++ app/src/main/java/com/augmentis/ayp/dnd/BoxDrawingView.java
@@ -0,0 +1,20 @@
+package com.augmentis.ayp.dnd;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Apinya on 8/30/2016.
+ */
+public class BoxDrawingView extends View{
+
+
+    public BoxDrawingView(Context context) {
+        super(context);
+    }
+
+    public BoxDrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
